5e4175d80833a3363ff2b5915c6e7a2191d99db9
yq00 <84113544+yq00@users.noreply.github.com>
2025-08-09 23:24:02 +0800

    Delete 250502/start.js

--- 250502/start.js
+++
@@ -1,4 +0,0 @@
-node --openssl-legacy-provider /Users/zhanaidarmukanov/Desktop/nft-game/node_modules/react-scripts/scripts/start.js
-/bin/sh -c ps cax | grep "Google Chrome Canary"
-/bin/sh -c ps cax | grep "Google Chrome"
-/bin/sh -c osascript openChrome.applescript "http://localhost:3000" "Google Chrome"
